a9bc969b46b863f28f47320d2da8b1b87c713736
SyuanTsai <carsun00@Gmail.com>
2025-02-09 21:49:10 +0800

    新增繁體中文

--- Warhammer 40,000 DARKTIDE/mods/DecodeMinigameSolver/scripts/DecodeMinigameSolver_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/DecodeMinigameSolver/scripts/DecodeMinigameSolver_localization.lua
@@ -11,24 +11,31 @@ return {
   mod_name =
   {
     en = "Decode Minigame Solver",
+    ["zh-tw"] = "自動解碼",
   },
   mod_description =
   {
-    en = "Passes the Decode Symbol Minigame automatically"
+    en = "Passes the Decode Symbol Minigame automatically",
+    ["zh-tw"] = "自動解碼小遊戲",
   },
   [SettingNames.EnableMod] = {
-    en = "Enable"
+    en = "Enable",
+    ["zh-tw"] = "啟動",
   },
   [SettingNames.InteractCooldown] = {
-    en = "Interaction Cooldown"
+    en = "Interaction Cooldown",
+    ["zh-tw"] = "互動冷卻時間",
   },
   [SettingNames.InteractCooldownTooltip] = {
-    en = "Wait time between interactions in milliseconds. Default: 100"
+    en = "Wait time between interactions in milliseconds. Default: 100",
+    ["zh-tw"] = "互動冷卻時間為毫秒 預設值為100",
   },
   [SettingNames.TargetPrecision] = {
-    en = "Target Precision"
+    en = "Target Precision",
+    ["zh-tw"] = "目標精確度",
   },
   [SettingNames.TargetPrecisionTooltip] = {
-    en = "The bigger the value, the smaller the detection window when the cursor is positioned on the symbol. Default: 3"
+    en = "The bigger the value, the smaller the detection window when the cursor is positioned on the symbol. Default: 3",
+    ["zh-tw"] = "當指標定位在符號上時 數值越大 偵測框越小 預設值為4",
   },
 }
