0305e51c621dd4f08b55b5796505b4aec63d808e
SyuanTsai <carsun00@Gmail.com>
2025-02-09 22:38:53 +0800

    新增繁體中文

--- Warhammer 40,000 DARKTIDE/mods/SuperImpact/scripts/mods/SuperImpact/SuperImpact_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/SuperImpact/scripts/mods/SuperImpact/SuperImpact_localization.lua
@@ -5,6 +5,7 @@ return {
 		fr = "Super Impact!",
 		ru = "Супер Удар!",
 		["zh-cn"] = "超级冲击!",
+		["zh-tw"] = "超級衝擊！",
 	},
 	mod_description = {
 		en = "Amplifies death blow impact on enemies. Ragdolls will go flying!",
@@ -12,6 +13,7 @@ return {
 		fr = "Amplifie l'impact des coups mortels sur les ennemis. Les ennemis voleront comme des poupées de chiffon!",
 		ru = "Усиливает ударную силу смертельных ударов по врагам. Враги будут разлетаться как куклы!",
 		["zh-cn"] = "增强对敌人致命一击的冲击力。尸体会飞出去!",
+		["zh-tw"] = "加強對敵人致命一擊的衝擊力，屍體會被擊飛！",
 	},
 	power = {
 		en = "Impact Power",
@@ -19,5 +21,6 @@ return {
 		fr = "Puissance d'impact",
 		ru = "Сила удара",
 		["zh-cn"] = "冲击力",
+		["zh-tw"] = "衝擊力",
 	},
 }
