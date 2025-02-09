7bd5075faeb57317ef6d85bb02a50581f3f071e5
SyuanTsai <carsun00@Gmail.com>
2025-02-09 21:41:20 +0800

    新增中文翻譯

--- Warhammer 40,000 DARKTIDE/mods/Decode_Helper/scripts/mods/Decode_Helper/Decode_Helper_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/Decode_Helper/scripts/mods/Decode_Helper/Decode_Helper_localization.lua
@@ -1,15 +1,19 @@
 return {
 	mod_name = {
 		en = "Decode Helper",
+		["zh-tw"] = "解碼助手",
 	},
 	mod_description = {
 		en = "Show upcoming target symbols when decoding.",
+		["zh-tw"] = "在解碼時顯示即將出現的目標符號。",
 	},
 	highlight_opacity = {
 		en = "Highlight Opacity",
+		["zh-tw"] = "高亮透明度",
 	},
 	reveal_count = {
 		en = "Amount of Revealed Rows",
+		["zh-tw"] = "顯示的行數",
 	},
 	num_1 = {
 		en = "1",
