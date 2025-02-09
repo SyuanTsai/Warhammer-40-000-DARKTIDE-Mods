bb4b854e23430ede1f6990d30a79d7993496b6c5
SyuanTsai <carsun00@Gmail.com>
2025-02-09 21:19:44 +0800

    新增中文翻譯

--- Warhammer 40,000 DARKTIDE/mods/more_characters_and_loadouts/scripts/mods/more_characters_and_loadouts/more_characters_and_loadouts_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/more_characters_and_loadouts/scripts/mods/more_characters_and_loadouts/more_characters_and_loadouts_localization.lua
@@ -1,8 +1,10 @@
 return {
 	mod_name = {
 		en = "More Characters and Loadouts",
+		["zh-tw"] = "擴充角色與配裝欄位", 
 	},
 	mod_description = {
 		en = "Increase the max number of characters and loadouts",
+		["zh-tw"] = "增加角色與配裝欄位的上限數量",
 	},
 }
