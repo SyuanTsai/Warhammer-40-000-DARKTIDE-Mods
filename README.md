6a66573bba79407669bee088a65d39fcc6ef4526
SyuanTsai <carsun00@Gmail.com>
2025-02-09 23:07:28 +0800

    新增StimmsPickupIcon

--- README.md
+++ README.md
@@ -118,13 +118,20 @@
 - MOD 檔案名稱：SuperImpact.zip-319-V1-1-1718836296
 - 手動維護最後下載日期：2025-02-09
 
-
 ### [AutoTag](https://www.nexusmods.com/warhammer40kdarktide/mods/374)
     自動標記 - 只要準心有對到菁英以上都會自動標記
 - MOD 網站最後更新日期：Last updated 10 October 2024 6:58PM
 - MOD 版本：1.6
 - MOD 檔案名稱：AutoTag.zip-374-V1-6-1728583087
 - 手動維護最後下載日期：2025-02-09
+
+### [Stimms Pickup Icon](https://www.nexusmods.com/warhammer40kdarktide/mods/265)
+    強化針顯示改善，讓你可以清楚知道隊友目前的針是甚麼
+- MOD 網站最後更新日期：Last updated 31 December 2024 12:36PM
+- MOD 版本：1.3.1
+- MOD 檔案名稱：StimmsPickupIcon-265-1-3-1-1735648580
+- 手動維護最後下載日期：2025-02-09
+
 
 ---
 # 遊戲內操作相關MOD
---
+++ Warhammer 40,000 DARKTIDE/mods/StimmsPickupIcon/scripts/mods/StimmsPickupIcon/StimmsPickupIcon_localization.lua
@@ -0,0 +1,32 @@
+return {
+	mod_name = {
+		en = "Stimms Pickup Icon",
+		["zh-cn"] = "兴奋剂拾取图标",
+		ru = "Цвет значков стимуляторов",
+	},
+	mod_description = {
+		en = "Set corresponding color for stimms icons.",
+		["zh-cn"] = "给兴奋剂图标设置对应的颜色。",
+		ru = "Stimms Pickup Icon - Установливает соответствующие цвета для значков стимуляторов.",
+	},
+	show_pickup_color = {
+		en = "Show colorful pickup icons",
+		["zh-cn"] = "显示彩色拾取图标",
+		ru = "Показать цветные значки поднимаемых",
+	},
+	show_team_panel_color = {
+		en = "Show colorful icons in team HUD",
+		["zh-cn"] = "在团队 HUD 中显示彩色图标",
+		ru = "Показывать цветные значки в командном интерфейсе",
+	},
+	show_self_weapon_color = {
+		en = "Show colorful icons in weapon HUD",
+		["zh-cn"] = "在武器 HUD 中显示彩色图标",
+		ru = "Показывать цветные иконки в интерфейсе оружия",
+	},
+	use_recolor_stimms_mod = {
+		en = "Use color settings of RecolorStimms mod",
+		["zh-cn"] = "使用 RecolorStimms 模组的颜色设置",
+		ru = "Использовать настройки цвета мода «Recolor Stimms»",
+	},
+}
