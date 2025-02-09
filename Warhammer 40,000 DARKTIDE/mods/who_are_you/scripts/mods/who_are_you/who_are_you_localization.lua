ca093d1aac29305c58c6165e005cb387f8db6af8
SyuanTsai <carsun00@Gmail.com>
2025-02-09 20:19:14 +0800

    新增繁體中文翻譯

--- Warhammer 40,000 DARKTIDE/mods/who_are_you/scripts/mods/who_are_you/who_are_you_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/who_are_you/scripts/mods/who_are_you/who_are_you_localization.lua
@@ -24,6 +24,7 @@ local loc = {
         en = "Who Are You",
         ru = "Кто ты",
         ["zh-cn"] = "到底是谁",
+        ["zh-tw"] = "你是誰",
     },
     mod_description = {
         en = "Display players' account name next to their character name.",
@@ -31,6 +32,7 @@ local loc = {
         ru = "Who Are You - Отображет имя учётной записи игрока рядом с именем его персонажа.",
         ["zh-cn"] = "在玩家的角色名字旁边显示账户名字。",
         fr = "Affiche le Nom du Compte des joueurs à coté du Nom de Personnage.",
+        ["zh-tw"] = "在玩家的角色名字旁邊顯示帳號名字。",
     },
     display_style = {
         en = "Display style",
@@ -38,6 +40,7 @@ local loc = {
         ru = "Стиль отображения",
         ["zh-cn"] = "显示样式",
         fr = "Style d'affichage",
+        ["zh-tw"] = "顯示方式",
     },
     character_first = {
         en = "Character Name (Account Name)",
@@ -45,6 +48,7 @@ local loc = {
         ru = "Имя персонажа (Учётная запись)",
         ["zh-cn"] = "角色名字（账户名字）",
         fr = "Nom du Personnage (Nom de Compte)",
+        ["zh-tw"] = "角色名字（帳號名字）",
     },
     account_first = {
         en = "Account Name (Character Name)",
@@ -52,6 +56,7 @@ local loc = {
         ru = "Учётная запись (Имя персонажа)",
         ["zh-cn"] = "账户名字（角色名字）",
         fr = "Nom de Compte (Nom du Personnage)",
+        ["zh-tw"] = "帳號名字（角色名字）",
     },
     character_only = {
         en = "Character Name",
@@ -59,6 +64,7 @@ local loc = {
         ru = "Имя персонажа",
         ["zh-cn"] = "角色名字",
         fr = "Nom du Personnage",
+        ["zh-tw"] = "角色名字",
     },
     account_only = {
         en = "Account Name",
@@ -66,6 +72,7 @@ local loc = {
         ru = "Учётная запись",
         ["zh-cn"] = "账户名字",
         fr = "Nom de Compte",
+        ["zh-tw"] = "帳號名字",
     },
     platform_icon = {
         en = "Platform Icon",
@@ -73,6 +80,7 @@ local loc = {
         ru = "Иконка платформы",
         ["zh-cn"] = "平台图标",
         fr = "Logo de la platforme",
+        ["zh-tw"] = "平台圖示",
     },
     off = {
         en = Localize("loc_setting_checkbox_off"),
@@ -83,6 +91,7 @@ local loc = {
         ru = "Скрыть тег игрока (4 цифры)",
         ["zh-cn"] = "隐藏 4 位数 ID 标签",
         fr = "Cacher le tag à 4 chiffres",
+        ["zh-tw"] ="隱藏四位數的ID標籤",
     },
     cycle_style = {
         en = "Cycle Style",
@@ -90,6 +99,7 @@ local loc = {
         ru = "Переключение стилей",
         ["zh-cn"] = "循环切换样式",
         fr = "Style de Cycle",
+        ["zh-tw"] = "循環切換樣式",
     },
     key_cycle_style = {
         en = "Keybind: Next style",
@@ -97,6 +107,7 @@ local loc = {
         ru = "Клавиша: следующий стиль",
         ["zh-cn"] = "快捷键：下一个样式",
         fr = "Raccourcis: Style suivant",
+        ["zh-tw"] = "快捷鍵：下一個樣式",
     },
     enable_cycle_notif = {
         en = "Notify current style",
@@ -104,6 +115,7 @@ local loc = {
         ru = "Уведомление о выбранном стиле",
         ["zh-cn"] = "通知提醒当前的样式",
         fr = "Afficher le Style actuel",
+        ["zh-tw"] = "通知當前樣式",
     },
     current_style = {
         en = "current style: ",
@@ -111,6 +123,7 @@ local loc = {
         ru = "текущий стиль: ",
         ["zh-cn"] = "当前样式：",
         fr = "Style Actuel",
+        ["zh-tw"] = "當前樣式：",
     },
     enable_display_self = {
         en = "Display your own account name",
@@ -118,6 +131,7 @@ local loc = {
         ru = "Видимость имени вашей учётной записи",
         ["zh-cn"] = "显示你自己的账户名字",
         fr = "Affiche seulement le Nom de Compte",
+        ["zh-tw"] = "顯示你自己的帳號名字",
     },
     modify_target = {
         en = "Applied to",
@@ -125,6 +139,7 @@ local loc = {
         ru = "К каким частям интерфейса применяется",
         ["zh-cn"] = "应用到",
         fr = "S'applique sur",
+        ["zh-tw"] = "應用到",
     },
     global = {
         en = "Global",
@@ -132,6 +147,7 @@ local loc = {
         ru = "Глобально",
         ["zh-cn"] = "全局",
         fr = "Global",
+        ["zh-tw"] = "全域",
     },
     enable_chat = {
         en = "Chat",
@@ -139,6 +155,7 @@ local loc = {
         ru = "Чат",
         ["zh-cn"] = "聊天栏",
         fr = "Discussion",
+        ["zh-tw"] = "聊天欄",
     },
     enable_combat_feed = {
         en = "Combat Feed",
@@ -146,6 +163,7 @@ local loc = {
         ru = "Лента убийств",
         ["zh-cn"] = "击杀通知栏",
         fr = "Diffusion de combat",
+        ["zh-tw"] = "戰鬥通知欄",
     },
     enable_inventory = {
         en = "Inventory",
@@ -153,6 +171,7 @@ local loc = {
         ru = "Инвентарь",
         ["zh-cn"] = "库存",
         fr = "Inventaire",
+        ["zh-tw"] = "物品欄",
     },
     enable_lobby = {
         en = "Lobby",
@@ -160,6 +179,7 @@ local loc = {
         ru = "Лобби",
         ["zh-cn"] = "小队大厅",
         fr = "Mourningstar",
+        ["zh-tw"] = "大廳",
     },
     enable_nameplate = {
         en = "Nameplate",
@@ -167,6 +187,7 @@ local loc = {
         ru = "Табличка с именем",
         ["zh-cn"] = "名称标签",
         fr = "Plaque de joueur",
+        ["zh-tw"] = "名稱標籤",
     },
     enable_inspect_player = {
         en = Localize("loc_lobby_entry_inspect"),
@@ -177,6 +198,7 @@ local loc = {
         ru = "Интерфейс команды",
         ["zh-cn"] = "团队 HUD",
         fr = "ATH d'Équipe",
+        ["zh-tw"] = "隊伍 HUD",
     },
     enable_group_finder = {
         en = Localize("loc_group_finder_menu_title")
@@ -187,6 +209,7 @@ local loc = {
         ru = "Настройки дополнительной части имени",
         ["zh-cn"] = "附加名字设置",
         fr = "Option du Nom Secondaire"
+        ["zh-tw"] = "附加名字設定",
     },
     tooltip_sub_name = {
         en = "This doesn't affect to chat name.",
@@ -194,6 +217,7 @@ local loc = {
         ru = "Это не влияет на имя в чате.",
         ["zh-cn"] = "这不会影响聊天名字。",
         fr = "N'affecte pas le nom de la disscusion",
+        ["zh-tw"] = "這不會影響聊天名字。",
     },
     enable_override = {
         en = "Override global settings",
@@ -201,6 +225,7 @@ local loc = {
         ru = "Заменить глобальные настройки",
         ["zh-cn"] = "覆盖全局设置",
         fr = "Ignorer les paramètres globaux",
+        ["zh-tw"] = "覆蓋全域設定",
     },
     enable_custom_size = {
         en = "Change sub name size",
@@ -208,6 +233,7 @@ local loc = {
         ru = "Изменить размер доп. имени",
         ["zh-cn"] = "更改附加名字大小",
         fr = "Changer la taille du Nom Secondaire",
+        ["zh-tw"] = "更改附加名字大小",
     },
     sub_name_size = {
         en = "Size",
@@ -215,6 +241,7 @@ local loc = {
         ru = "Размер",
         ["zh-cn"] = "大小",
         fr = "Taille",
+        ["zh-tw"] = "大小",
     },
     enable_custom_color = {
         en = "Change sub name color",
@@ -222,6 +249,7 @@ local loc = {
         ru = "Изменить цвет доп. имени",
         ["zh-cn"] = "更改附加名字颜色",
         fr = "Changer la couleur du Nom Secondaire",
+        ["zh-tw"] = "更改附加名字顏色",
     },
     custom_color = {
         en = "Custom color",
@@ -229,24 +257,28 @@ local loc = {
         ru = "Пользовательские цвета",
         ["zh-cn"] = "自定义颜色",
         fr = "Couleur personnalisée",
+        ["zh-tw"] = "自訂顏色",
     },
     color_r = {
         en = "R",
         ru = "Красный",
         ["zh-cn"] = "红",
         fr = "Rouge",
+        ["zh-tw"] = "紅",
     },
     color_g = {
         en = "G",
         ru = "Зелёный",
         ["zh-cn"] = "绿",
         fr = "Vert",
+        ["zh-tw"] = "綠",
     },
     color_b = {
         en = "B",
         ru = "Синий",
         ["zh-cn"] = "蓝",
         fr = "Bleu",
+        ["zh-tw"] = "藍",
     },
 }
 
