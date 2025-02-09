a2c3e62f20bc02480d3f058a3682d492a9ca8f82
SyuanTsai <carsun00@Gmail.com>
2025-02-09 22:02:28 +0800

    新增繁體中文

--- Warhammer 40,000 DARKTIDE/mods/Tree_Helper/scripts/mods/Tree_Helper/Tree_Helper_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/Tree_Helper/scripts/mods/Tree_Helper/Tree_Helper_localization.lua
@@ -1,8 +1,10 @@
 return {
 	mod_name = {
 		en = "Tree Helper",
+		en = "毒樹助手",
 	},
 	mod_description = {
 		en = "Show upcoming target symbols when decoding Tree Scans",
+		["zh-tw"] = "在解碼樹木掃描時顯示即將出現的目標符號",
 	},
 }
