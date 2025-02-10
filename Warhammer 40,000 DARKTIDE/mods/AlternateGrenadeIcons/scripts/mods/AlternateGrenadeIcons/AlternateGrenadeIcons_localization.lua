dbdc8791ddae15225dd96faf18739d3053306d01
SyuanTsai <carsun00@Gmail.com>
2025-02-10 23:17:41 +0800

    新增繁體中文

--- Warhammer 40,000 DARKTIDE/mods/AlternateGrenadeIcons/scripts/mods/AlternateGrenadeIcons/AlternateGrenadeIcons_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/AlternateGrenadeIcons/scripts/mods/AlternateGrenadeIcons/AlternateGrenadeIcons_localization.lua
@@ -1,16 +1,20 @@
 return {
 	mod_name = {
 		en = "Alternate Grenade Icons",
+		["zh-tw"] = "替代手榴彈圖示",
 	},
 	mod_description = {
 		en = "Allows changing or hiding the grenade icon in the teammate HUD depending on what grenade they are using",
+		["zh-tw"] = "可根據隊友所使用的手榴彈類型，在隊友介面中更改或隱藏手榴彈圖示",
 	},
 	-- Options
 	default = {
 		en = "Grenade (default icon)",
+		["zh-tw"] = "手榴彈（預設圖示）",
 	},
 	hidden = {
 		en = "Hidden (no icon)",
+		["zh-tw"] = "隱藏（無圖示）",
 	},
 	i = {
 		en = "I",
@@ -29,70 +33,90 @@ return {
 	},
 	lightning_bolt = {
 		en = "Lightning bolt",
+		["zh-tw"] = "閃電圖示",
 		-- Not working: \u{26A1} ⚡︎ϟ⚡🗲 
 	},
 	explosion = {
 		en = "Explosion",
+		["zh-tw"] = "爆炸",
 	},
 	flame = {
 		en = "Flame",
+		["zh-tw"] = "火焰",
 	},
 	-- Ammo setting
 	hide_psyker_ammo = {
 		en = "Hide ammo icon for Psyker staves",
+		["zh-tw"] = "對靈能者法杖隱藏彈藥圖示",
 	},
 	-- Classes
 	widget_ogryn = {
 		en = "Ogryn",
+		["zh-tw"] = "歐格林",
 	},
 	widget_psyker = {
 		en = "Psyker",
+		["zh-tw"] = "靈能者",
 	},
 	widget_veteran = {
 		en = "Veteran",
+		["zh-tw"] = "老兵",
 	},
 	widget_zealot = {
 		en = "Zealot",
+		["zh-tw"] = "狂信徒",
 	},
 	-- Grenades
 	-- Ogryn
 	ogryn_grenade_friend_rock = {
 		en = "Rock",
+		["zh-tw"] = "石塊",
 	},
 	ogryn_grenade_box_cluster = {
 		en = "Box",
+		["zh-tw"] = "箱子",
 	},
 	ogryn_grenade_frag = {
 		en = "Nuke",
+		["zh-tw"] = "核彈",
 	},
 	-- Psyker
 	psyker_smite = {
 		en = "Brain burst",
+		["zh-tw"] = "顱爆",
 	},
 	psyker_chain_lightning = {
 		en = "Smite",
+		["zh-tw"] = "電手",
 	},
 	psyker_throwing_knives = {
 		en = "Assail",
+		["zh-tw"] = "飛石",
 	},
 	-- Veteran
 	veteran_frag_grenade = {
 		en = "Frag",
+		["zh-tw"] = "破片",
 	},
 	veteran_krak_grenade = {
 		en = "Krak",
+		["zh-tw"] = "穿甲",
 	},
 	veteran_smoke_grenade = {
 		en = "Smoke",
+		["zh-tw"] = "煙霧",
 	},
 	-- Zealot 
 	zealot_shock_grenade = {
 		en = "Stun",
+		["zh-tw"] = "震盪",
 	},
 	zealot_fire_grenade = {
 		en = "Fire",
+		["zh-tw"] = "火焰",
 	},
 	zealot_throwing_knives = {
 		en = "Throwing knives",
+		["zh-tw"] = "飛刀",
 	},
 }
