d544fef73a9c551624eb25a7dfb637c08375f25a
SyuanTsai <carsun00@Gmail.com>
2025-02-11 23:26:36 +0800

    新增繁體中文

--- Warhammer 40,000 DARKTIDE/mods/markers_aio/scripts/mods/markers_aio/heretical_idol_markers/heretical_idol_marker_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/markers_aio/scripts/mods/markers_aio/heretical_idol_markers/heretical_idol_marker_localization.lua
@@ -2,46 +2,59 @@ return {
     mod_name = {
         en = "Heretical Idol Marker",
         ru = "Отметка еретических идолов",
+        ["zh-tw"] = "異端雕像標記",
     },
     mod_description = {
         en = "Adds in-world icons to heretical idols for easier tracking.",
         ru = "Heretical Idol Marker - Добавляет внутриигровые значки еретическим идолам для более лёгкого отслеживания.",
+		["zh-tw"] = "為了更容易追蹤，將異端雕像添加到世界圖標中。",
     },
 
 	general_settings = {
         en = "General Settings",
+        ["zh-tw"] = "一般設定",
     },
     keep_on_screen = {
         en = "Keep on screen",
         ru = "Держать на экране",
+        ["zh-tw"] = "保持顯示於螢幕",
     },
     require_line_of_sight = {
         en = "Require line of sight",
         ru = "Требуется прямая видимость",
+        ["zh-tw"] = "需要視線範圍",
     },
     max_distance = {
-		en = "Max distance"
+		en = "Max distance",
+        ["zh-tw"] = "最遠距離",
 	},
 	max_size = {
-		en = "Maximum size of marker"
+		en = "Maximum size of marker",
+        ["zh-tw"] = "標記的最大尺寸",
 	},
 	min_size = {
-		en = "Minimum size of marker"
+		en = "Minimum size of marker",
+        ["zh-tw"] = "標記的最小尺寸",
 	},
 	alpha = {
-		en = "Alpha Multiplier"
+		en = "Alpha Multiplier",
+        ["zh-tw"] = "透明度倍增器",
 	},
 
 	icon_colour = {
-		en = "Icon Colour"
+		en = "Icon Colour",
+        ["zh-tw"] = "圖示顏色",
 	},
 	icon_colour_R = {
-		en = "R"
+		en = "R",
+        ["zh-tw"] = "紅",
 	},
 	icon_colour_G = {
-		en = "G"
+		en = "G",
+        ["zh-tw"] = "綠",
 	},
 	icon_colour_B = {
-		en = "B"
+		en = "B",
+        ["zh-tw"] = "藍",
 	},
 }
--- Warhammer 40,000 DARKTIDE/mods/markers_aio/scripts/mods/markers_aio/stimm_markers/stimm_markers_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/markers_aio/scripts/mods/markers_aio/stimm_markers/stimm_markers_localization.lua
@@ -1,82 +1,108 @@
 return {
     mod_name = {
         en = "Stimm Markers",
+        ["zh-tw"] = "興奮劑標記",
     },
     mod_description = {
         en = "Edits stimm markers to be easily distinguishable, and provides options for fully customising them.",
+		["zh-tw"] = "編輯興奮劑標記，使其易於辨識，並提供完全自定義的選項。",
     },
 
 	general_settings = {
         en = "General Settings",
+        ["zh-tw"] = "一般設定",
     },
     keep_on_screen = {
         en = "Keep on screen",
+        ["zh-tw"] = "保持顯示於螢幕",
     },
     require_line_of_sight = {
         en = "Require line of sight",
+        ["zh-tw"] = "需要視線範圍",
     },
-	max_distance = {
-		en = "Max distance"
+    max_distance = {
+		en = "Max distance",
+        ["zh-tw"] = "最遠距離",
 	},
 	max_size = {
-		en = "Maximum size of marker"
+		en = "Maximum size of marker",
+        ["zh-tw"] = "標記的最大尺寸",
 	},
 	min_size = {
-		en = "Minimum size of marker"
+		en = "Minimum size of marker",
+        ["zh-tw"] = "標記的最小尺寸",
 	},
 	alpha = {
-		en = "Alpha Multiplier"
+		en = "Alpha Multiplier",
+        ["zh-tw"] = "透明度倍增器",
 	},
+	
 
 	boost_stim_icon_colour = {
-		en = "Boost Stimm Icon Colour"
+		en = "Boost Stimm Icon Colour",
+        ["zh-tw"] = "增強興奮劑圖示顏色",
 	},
 	boost_stim_icon_colour_R = {
-		en = "R"
+		en = "R",
+        ["zh-tw"] = "紅",
 	},
 	boost_stim_icon_colour_G = {
-		en = "G"
+		en = "G",
+        ["zh-tw"] = "綠",
 	},
 	boost_stim_icon_colour_B = {
-		en = "B"
+		en = "B",
+        ["zh-tw"] = "藍",
 	},
 
 	corruption_stimm_icon_colour = {
-		en = "Corruption Stimm Icon Colour"
+		en = "Corruption Stimm Icon Colour",
+        ["zh-tw"] = "治療針圖示顏色",
 	},
 	corruption_stimm_icon_colour_R = {
-		en = "R"
+		en = "R",
+        ["zh-tw"] = "紅",
 	},
 	corruption_stimm_icon_colour_G = {
-		en = "G"
+		en = "G",
+        ["zh-tw"] = "綠",
 	},
 	corruption_stimm_icon_colour_B = {
-		en = "B"
+		en = "B",
+        ["zh-tw"] = "藍",
 	},
 
 	power_stimm_icon_colour = {
-		en = "Power Stimm Icon Colour"
+		en = "Power Stimm Icon Colour",
+        ["zh-tw"] = "戰鬥興奮劑圖示顏色",
 	},
 	power_stimm_icon_colour_R = {
-		en = "R"
+		en = "R",
+        ["zh-tw"] = "紅",
 	},
 	power_stimm_icon_colour_G = {
-		en = "G"
+		en = "G",
+        ["zh-tw"] = "綠",
 	},
 	power_stimm_icon_colour_B = {
-		en = "B"
+		en = "B",
+        ["zh-tw"] = "藍",
 	},
 
 	speed_stimm_icon_colour = {
-		en = "Speed Stimm Icon Colour"
+		en = "Speed Stimm Icon Colour",
+        ["zh-tw"] = "速度興奮劑圖示顏色",
 	},
 	speed_stimm_icon_colour_R = {
-		en = "R"
+		en = "R",
+        ["zh-tw"] = "紅",
 	},
 	speed_stimm_icon_colour_G = {
-		en = "G"
+		en = "G",
+        ["zh-tw"] = "綠",
 	},
 	speed_stimm_icon_colour_B = {
-		en = "B"
+		en = "B",
+        ["zh-tw"] = "藍",
 	},
 }
