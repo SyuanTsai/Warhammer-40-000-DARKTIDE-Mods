65aa496e095df29ecde9f2763681c7f69696aee4
SyuanTsai <carsun00@Gmail.com>
2025-02-24 00:44:39 +0800

    新增翻譯

--- Warhammer 40,000 DARKTIDE/mods/PlayerOutlines/loc.lua
+++ Warhammer 40,000 DARKTIDE/mods/PlayerOutlines/loc.lua
@@ -3,50 +3,60 @@ return {
 		en = "Player Outlines",
 		ru = "Контуры игроков",
 		["zh-cn"] = "玩家边框轮廓",
+		["zh-tw"] = "玩家邊框輪廓",
 	},
 	mod_description = {
 		en = "Enables you to optionally show the default skeleton-hologram, a highlighted mesh, and an outline, in any combination. The mesh is visible through walls only, but the outline is always visible. Enjoy!",
 		["zh-cn"] = "你可以显示默认的玩家全息骨架、高亮模型、高亮轮廓或者以上任意组合。高亮模型只能隔墙显示，但轮廓始终可见。",
 		ru = "Позволяет вам опционально отображать стандартную скелет-голограмму, подсвеченный меш и контур в любой комбинации. Меш виден только сквозь стены, но контур виден всегда. Наслаждайтесь!",
+		["zh-tw"] = "你可以顯示默認的玩家全息骨架、高亮模型、高亮輪廓或者以上任意組合。高亮模型只能隔牆顯示，但輪廓始終可見。",
 	},
 	show_hologram = {
 		en = "Show Hologram",
 		["zh-cn"] = "显示全息骨架",
 		ru = "Показывать голограмму",
+		["zh-tw"] = "顯示全息骨架",
 	},
 	show_mesh = {
 		en = "Show Mesh",
 		["zh-cn"] = "显示模型",
 		ru = "Показывать меш",
+		["zh-tw"] = "顯示模型",
 	},
 	show_outline = {
 		en = "Show Outline",
 		["zh-cn"] = "显示轮廓",
 		ru = "Показывать контур",
+		["zh-tw"] = "顯示輪廓",
 	},
 	show_in_hub = {
 		en = "Show in Mourningstar",
 		["zh-cn"] = "在哀星号上显示",
 		ru = "Показывать в Моунингстар",
+		["zh-tw"] = "在哀星號上顯示",
 	},
 	mission_nameplates_max_distance_title = {
 		en = "Nameplates Max. Distance",
 		["zh-cn"] = "名称标签最大距离",
 		ru = "Имена максимальное расстояние",
+		["zh-tw"] = "名字最大距離",
 	},
 	mission_nameplates_max_distance_tt = {
 		en = "Sets the maximum distance at which players' names will be visible above them during missions. Defaults to 500. Set to zero to hide them entirely.",
 		["zh-cn"] = "设置玩家头顶名称的最大显示距离。默认为 500。设置为 0 表示完全隐藏。",
 		ru = "Устанавливает максимальное расстояние, на котором имена игроков будут видны над ними во время миссий. По умолчанию 500. Установите ноль, чтобы скрыть их полностью.",
+		["zh-tw"] = "設置玩家頭頂名稱的最大顯示距離。默認為 500。設置為 0 表示完全隱藏。",
 	},
 	assist_marker_max_distance_title = {
 		en = "Assist Marker Max. Distance",
 		["zh-cn"] = "协助标签最大距离",
 		ru = "Маркер помощи макс. расстояние",
+		["zh-tw"] = "協助標籤最大距離",
 	},
 	assist_marker_max_distance_tt = {
 		en = "Sets the maximum distance at which players' assist markers will be visible above them when the player is down. Defaults to 200. Set to zero to hide them entirely.",
 		["zh-cn"] = "设置玩家倒地时的协助图标最大显示距离。默认为 200。设置为 0 表示完全隐藏。",
 		ru = "Устанавливает максимальное расстояние, на котором маркеры помощи игрокам будут видны над ними, когда игрок лежит. По умолчанию 200. Установите ноль, чтобы скрыть их полностью.",
+		["zh-tw"] = "設置玩家倒地時的協助圖標最大顯示距離。默認為 200。設置為 0 表示完全隱藏。",
 	}
 }
