a1089a183787a68898d13219912a4492d77f0135
SyuanTsai <carsun00@Gmail.com>
2025-02-24 00:53:59 +0800

    新增Guarantee Ability Activation

---
+++ Warhammer 40,000 DARKTIDE/mods/guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation_localization.lua
@@ -0,0 +1,45 @@
+-- ru localization by xsSplater
+-- zh-cn localization by deluxghost
+
+return {
+	mod_name = {
+		en = "Guarantee Ability Activation",
+		ru = "Гарантированная активация способности",
+		["zh-cn"] = "保证技能激活",
+	},
+	mod_description = {
+		en = "Guarantee combat ability activation when player try to activate them in middle of certain animations",
+		ru = "Гарантированная активация боевой способности, когда игрок пытается активировать её в середине анимации",
+		["zh-cn"] = "确保玩家在动画中途能成功激活主动技能",
+	},
+	enable_prevent_double_dashing = {
+		en = "Prevent double dashing",
+		ru = "Предотвращение двойного рывка",
+		["zh-cn"] = "防止二次冲锋",
+	},
+	enable_prevent_double_dashing_description = {
+		en = "Blocking ability key press while dashing",
+		ru = "Блокировка нажатия клавиши способности во время рывка",
+		["zh-cn"] = "在冲锋过程中阻止技能键",
+	},
+	enable_prevent_relic_cancel = {
+		en = "Prevent chorus cancel from ability key",
+		ru = "Предотвращение отмены Хора при нажатии клавиши способности",
+		["zh-cn"] = "防止按下技能键时取消圣咏",
+	},
+	enable_prevent_ability_aiming = {
+		en = "Disable ability holding feature",
+		ru = "Отключение функции удержания способности",
+		["zh-cn"] = "禁用技能键按住功能",
+	},
+	debug_group = {
+		en = "Debug",
+		ru = "Отладка",
+		["zh-cn"] = "调试",
+	},
+	enable_debug_modding_tools = {
+		en = "Modding Tools",
+		ru = "Инструменты модификации",
+		["zh-cn"] = "模组开发者工具",
+	},
+}
