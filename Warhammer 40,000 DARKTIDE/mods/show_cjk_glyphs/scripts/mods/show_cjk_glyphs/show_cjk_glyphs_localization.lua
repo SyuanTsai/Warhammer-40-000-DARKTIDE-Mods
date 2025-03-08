a04134b5be0916adc74e7795b3bdcc741947e18d
SyuanTsai <carsun00@Gmail.com>
2025-03-09 00:57:14 +0800

    新增翻譯

--- Warhammer 40,000 DARKTIDE/mods/show_cjk_glyphs/scripts/mods/show_cjk_glyphs/show_cjk_glyphs_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/show_cjk_glyphs/scripts/mods/show_cjk_glyphs/show_cjk_glyphs_localization.lua
@@ -2,29 +2,36 @@ return {
     mod_name = {
         en = "Show CJK Glyphs",
         ["zh-cn"] = "显示 CJK 字形",
+        ["zh-tw"] = "顯示 CJK 字形",
     },
     mod_description = {
         en = "Show CJK glyphs.",
         ["zh-cn"] = "显示 CJK 字形。",
+        ["zh-tw"] = "顯示 CJK 字形。",
     },
     lang_priority = {
         en = "Language Priority",
         ["zh-cn"] = "语言优先级",
+        ["zh-tw"] = "語言優先級",
     },
     q_zh_hans = {
         en = "Chinese (Simplified)",
         ["zh-cn"] = "中文（简体）",
+        ["zh-tw"] = "中文（簡體）",
     },
     q_zh_hant = {
         en = "Chinese (Traditional)",
         ["zh-cn"] = "中文（繁体）",
+        ["zh-tw"] = "中文（繁體）",
     },
     q_ja = {
         en = "Japanese",
         ["zh-cn"] = "日文",
+        ["zh-tw"] = "日文",
     },
     q_ko = {
         en = "Korean",
         ["zh-cn"] = "韩文",
+        ["zh-tw"] = "韓文",
     },
 }
