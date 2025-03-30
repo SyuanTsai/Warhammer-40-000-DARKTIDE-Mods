901c6ba1d0f24bfb218a93d99ffadb391b495430
SyuanTsai <carsun00@Gmail.com>
2025-03-30 21:40:40 +0800

    新增翻譯

--- Warhammer 40,000 DARKTIDE/mods/RecolorBossHealthBars/scripts/mods/RecolorBossHealthBars/RecolorBossHealthBars_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/RecolorBossHealthBars/scripts/mods/RecolorBossHealthBars/RecolorBossHealthBars_localization.lua
@@ -1,21 +1,27 @@
 local loc = {
 	mod_name = {
         en = "Recolor Boss Health Bars",
+		["zh-tw"] = "重繪Boss血條",
     },
 	mod_description = {
 		en = "Recolors the health bars of certain enemies",
+		["zh-tw"] = "重繪某些敵人的血條",
 	},
 	color_daemonhost = {
 		en = "Daemonhost",
+		["zh-tw"] = "惡魔宿主",
 	},
 	color_weakened = {
 		en = "Weakened monsters",
+		["zh-tw"] = "虛弱的怪物",
 	},
 	color_others = {
 		en = "Other monsters",
+		["zh-tw"] = "其他怪物",
 	},
 	tooltip_color_toggle = {
 		en = "\nUse the specified color instead of the color defined in \"Other monsters\"",
+		["zh-tw"] = "\n使用指定顏色，而不是“其他怪物”中定義的顏色",
 	},
 }
 
