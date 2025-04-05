dd3c20bebe046015edfe2a922aecea03e043c720
SyuanTsai <carsun00@Gmail.com>
2025-04-05 19:47:47 +0800

    新增翻譯

--- Warhammer 40,000 DARKTIDE/mods/afk/scripts/mods/afk/afk_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/afk/scripts/mods/afk/afk_localization.lua
@@ -2,21 +2,25 @@ return {
   mod_name = {
     en = "AFK",
     ["zh-cn"] = "挂机模式",
+    ["zh-tw"] = "AFK",
     ru = "AFK",    
   },
   mod_description = {
     en = "Delays the server AFK timer for a set number of checks.",
     ["zh-cn"] = "延后一定次数的服务器挂机检查。",
+    ["zh-tw"] = "延遲伺服器AFK計時器的檢查次數",
     ru = "Откладывает AFK-таймер сервера на заданное количество проверок.",
   },
   block_check_limit = {
     en = "Block Check Limit",
     ["zh-cn"] = "阻止检查上限",
+    ["zh-tw"] = "阻止檢查上限",
     ru = "Количество проверок",
   },
   block_check_limit_description = {
     en = "Maximum number of prevented checks.",
     ["zh-cn"] = "阻止挂机检查的最大次数。",
+    ["zh-tw"] = "阻止檢查的最大次數",
     ru = "Максимальное количество пропущенных проверок.",
   },
 }
