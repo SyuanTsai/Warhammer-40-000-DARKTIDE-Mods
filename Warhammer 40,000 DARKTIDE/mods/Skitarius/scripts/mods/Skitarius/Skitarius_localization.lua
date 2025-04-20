8fefabef13526cb43af258b8af63921e9b4b1693
SyuanTsai <carsun00@Gmail.com>
2025-04-20 18:18:29 +0800

    Add Skitarius

---
+++ Warhammer 40,000 DARKTIDE/mods/Skitarius/scripts/mods/Skitarius/Skitarius_localization.lua
@@ -0,0 +1,568 @@
+return {
+	-- Mod Details
+	mod_name = {
+		en = "Skitarius",
+	},
+	mod_description = {
+		en = "FROM THE MOMENT I UNDERSTOOD THE WEAKNESS OF MY FLESH, IT DISGUSTED ME.",
+	},
+	-- Debug
+	debug = {
+		en = "Debug",
+	},
+	-- Keybinds
+	override_primary = {
+		en = "Override Primary Attack Input",
+	},
+	keybinds = {
+		en = "Keybinds",
+	},
+	keybind_selection_melee = {
+		en = "Keybind Selection",
+	},
+	keybind_selection_ranged = {
+		en = "Keybind Selection",
+	},
+	keybind_one_pressed = {
+		en = "Keybind One (Toggle)",
+	},
+	keybind_one_held = {
+		en = "Keybind One (Held)",
+	},
+	keybind_two_pressed = {
+		en = "Keybind Two (Toggle)",
+	},
+	keybind_two_held = {
+		en = "Keybind Two (Held)",
+	},
+	keybind_three_pressed = {
+		en = "Keybind Three (Toggle)",
+	},
+	keybind_three_held = {
+		en = "Keybind Three (Held)",
+	},
+	keybind_four_pressed = {
+		en = "Keybind Four (Toggle)",
+	},
+	keybind_four_held = {
+		en = "Keybind Four (Held)",
+	},
+	-- Melee
+	melee_settings = {
+		en = "Melee Settings",
+	},
+	melee_weapon_selection = {
+		en = "Weapon Selection",
+	},
+	heavy_buff = {
+		en = "Heavy Buff Modifier"
+	},
+	thrust = {
+		en = "Thrust"
+	},
+	slow_and_steady = {
+		en = "Slow and Steady"
+	},
+	crunch = {
+		en = "Crunch"
+	},
+	heavy_buff_stacks = {
+		en = "Heavy Buff Stacks"
+	},
+	heavy_buff_special = {
+		en = "Special Required For Heavy Modifier"
+	},
+	global_melee = {
+		en = "GLOBAL",
+	},
+	sequence_cycle_point = {
+		en = "Cycle Point",
+	},
+	sequence_step_one = {
+		en = "Step One",
+	},
+	sequence_step_two = {
+		en = "Step Two",
+	},
+	sequence_step_three = {
+		en = "Step Three",
+	},
+	sequence_step_four = {
+		en = "Step Four",
+	},
+	sequence_step_five = {
+		en = "Step Five",
+	},
+	sequence_step_six = {
+		en = "Step Six",
+	},
+	-- Sequence steps
+	none = {
+		en = "None",
+	},
+	light_attack = {
+		en = "Light Attack",
+	},
+	heavy_attack = {
+		en = "Heavy Attack",
+	},
+	special_action = {
+		en = "Special Action",
+	},
+	block = {
+		en = "Block",
+	},
+	push = {
+		en = "Push",
+	},
+	push_attack = {
+		en = "Push Attack",
+	},
+	-- Reset
+	reset_weapon_melee = {
+		en = "RESET MELEE WEAPON",
+	},
+	reset_all_melee = {
+		en = "RESET ALL MELEE SETTINGS",
+	},
+	reset_weapon_ranged = {
+		en = "RESET RANGED WEAPON",
+	},
+	reset_all_ranged = {
+		en = "RESET ALL RANGED SETTINGS",
+	},
+	-- Ranged
+	ranged_settings = {
+		en = "Ranged Settings",
+	},
+	always_charge = {
+		en = "Always Auto-Release Charges",
+	},
+	always_charge_tooltip = {
+		en = string.format("Automatically release charged attacks when the charge is full, regardless of other ranged settings."),
+	},
+	ranged_weapon_selection = {
+		en = "Weapon Selection",
+	},
+	global_ranged = {
+		en = "GLOBAL",
+	},
+	automatic_fire = {
+		en = "Automatic Fire",
+	},
+	standard = {
+		en = "Standard",
+	},
+	charged = {
+		en = "Charged",
+	},
+	special = {
+		en = "Special",
+	},
+	auto_charge_threshold = {
+		en = "Charge Threshold %%",
+	},
+	auto_shoot = {
+		en = "Shoot Without Input",
+	},
+	ads_filter = {
+		en = "ADS/Hipfire Filter",
+	},
+	ads_only = {
+		en = "ADS Only",
+	},
+	ads_hip = {
+		en = "ADS and Hipfire",
+	},
+	hip_only = {
+		en = "Hipfire Only",
+	},
+	rate_of_fire = {
+		en = "Rate of Fire",
+	},
+	rate_of_fire_hip = {
+		en = "Rate of Fire %% (Hipfire)",
+	},
+	rate_of_fire_ads = {
+		en = "Rate of Fire %% (ADS)",
+	},
+	automatic_special = {
+		en = "Automatic Special",
+	},
+	-- WEAPONS
+	-- MELEE
+	ogryn_combatblade_p1_m1 = {
+		en = "Krourk Mk VI Cleaver",
+		["zh-tw"] = "VI(六)型砍刀(克魯克)",
+	},
+	ogryn_combatblade_p1_m2 = {
+		en = "Bull Butcher Mk III Cleaver",
+		["zh-tw"] = "III(三)型砍刀(蠻牛屠夫)",
+	},
+	ogryn_combatblade_p1_m3 = {
+		en = "Krourk Mk IV Cleaver",
+		["zh-tw"] = "IV(四)型砍刀(克魯克)",
+	},
+	ogryn_gauntlet_p1_m1 = {
+		en = "Blastoom Mk III Grenadier Gauntlet",
+		["zh-tw"] = "III(三)型擲彈兵臂鎧(布拉斯托姆)",
+	},
+	ogryn_club_p1_m1 = {
+		en = "Brute-Brainer Mk III Latrine Shovel",
+		["zh-tw"] = "III(三)型廁所鏟(兇殘)",
+	},
+	ogryn_club_p1_m2 = {
+		en = "Brute-Brainer Mk XIX Latrine Shovel",
+		["zh-tw"] = "XIX(十九)型廁所鏟(兇殘)",
+	},
+	ogryn_club_p1_m3 = {
+		en = "Brute-Brainer Mk V Latrine Shovel",
+		["zh-tw"] = "V(五)型廁所鏟(兇殘)",
+	},
+	ogryn_club_p2_m1 = {
+		en = "Brunt Special Mk I Bully Club",
+		["zh-tw"] = "I(一)型惡霸棍棒(布倫特專用)",
+	},
+	ogryn_club_p2_m2 = {
+		en = "Brunt's Pride Mk II Bully Club",
+		["zh-tw"] = "II(二)型惡霸棍棒(布倫特得意之作)",
+	},
+	ogryn_club_p2_m3 = {
+		en = "Brunt's Basher Mk IIIb Bully Club",
+		["zh-tw"] = "IIIb(3B)型惡霸棍棒(布倫特猛擊)",
+	},
+	ogryn_pickaxe_2h_p1_m1 = {
+		en = "Branx Mk Ia Pickaxe",
+		["zh-tw"] = "Ia(1A)型戴維爾戰鎬(布蘭克斯)",
+	},
+	ogryn_pickaxe_2h_p1_m2 = {
+		en = "Borovian Mk III Pickaxe",
+		["zh-tw"] = "III(三)型戴維爾戰鎬(博羅維安)",
+	},
+	ogryn_pickaxe_2h_p1_m3 = {
+		en = "Karsolas Mk II Pickaxe",
+		["zh-tw"] = "II(二)型戴維爾戰鎬(卡索拉斯)",
+	},
+	ogryn_powermaul_p1_m1 = {
+		en = "Achlys Mk I Power Maul",
+		["zh-tw"] = "I(一)型動力鎚(阿克利斯)",
+	},
+	--[[ THESE WEAPONS AREN'T ACCESSIBLE IN-GAME YET
+	ogryn_powermaul_p1_m2 = {
+		en = "Ogrys Mk IIc Power Maul",
+	},
+	ogryn_powermaul_p1_m3 = {
+		en = "??? Mk ??? Power Maul",
+	},
+	--]]
+	ogryn_powermaul_slabshield_p1_m1 = {
+		en = "Orox Mk II & Mk III Battle Maul & Slab Shield",
+		["zh-tw"] = "Orox 第II(二)型 與 第III(三)型作戰大鎚與板盾",
+	},
+	forcesword_p1_m1 = {
+		en = "Obscurus Mk II Blaze Force Sword",
+		["zh-tw"] = "II(二)型烈焰力場劍(朦朧)",
+	},
+	forcesword_p1_m2 = {
+		en = "Deimos Mk IV Blaze Force Sword",
+		["zh-tw"] = "IV(四)型烈焰力場劍(戴莫斯)",
+	},
+	forcesword_p1_m3 = {
+		en = "Illsi Mk V Blaze Force Sword",
+		["zh-tw"] = "V(五)型烈焰力場劍(伊利斯))",
+	},
+	forcesword_2h_p1_m1 = {
+		en = "Covenant Mk VI Blaze Force Greatsword",
+		["zh-tw"] = "VI(六)型烈焰力場巨劍(誓約)",
+	},
+	forcesword_2h_p1_m2 = {
+		en = "Covenant Mk VIII Blaze Force Greatsword",
+		["zh-tw"] = "VIII(八)型烈焰力場巨劍(誓約)",
+	},
+	-- VETERAN ONLY
+	powersword_p1_m1 = {
+		en = "Scandar Mk III Power Sword",
+		["zh-tw"] = "III(三)型動力劍(斯干達)",
+	},
+	powersword_p1_m2 = {
+		en = "Achlys Mk VI Power Sword",
+		["zh-tw"] = "VI(六)型動力劍(阿克利斯)",
+	},
+	combataxe_p3_m1 = {
+		en = "Munitorum Mk I Sapper Shovel",
+		["zh-tw"] = "I(一)型工兵鏟(軍務部)",
+	},
+	combataxe_p3_m2 = {
+		en = "Munitorum Mk III Sapper Shovel",
+		["zh-tw"] = "III(三)型工兵鏟(軍務部)",
+	},
+	combataxe_p3_m3 = {
+		en = "Munitorum Mk VII Sapper Shovel",
+		["zh-tw"] = "VII(七)型工兵鏟(軍務部)",
+	},
+	-- ZEALOT ONLY
+	chainsword_2h_p1_m1 = {
+		en = "Tigrus Mk III Heavy Eviscerator",
+		["zh-tw"] = "III(三)型重型開膛劍(泰格魯斯)",
+	},
+	chainsword_2h_p1_m2 = {
+		en = "Tigrus Mk XV Heavy Eviscerator",
+		["zh-tw"] = "XV(十五)型重型開膛劍(泰格魯斯)",
+	},
+	powermaul_2h_p1_m1 = {
+		en = "Indignatus Mk IVe Crusher",
+		["zh-tw"] = "IVe(4E)型碾壓者(憤怒)",
+	},
+	powersword_2h_p1_m1 = {
+		en = "Munitorum Mk X Relic Blade",
+		["zh-tw"] = "X(十)型上古神刃(軍務部)",
+	},
+	powersword_2h_p1_m2 = {
+		en = "Munitorum Mk II Relic Blade",
+		["zh-tw"] = "II(二)型上古神刃(軍務部)",
+	},
+	thunderhammer_2h_p1_m1 = {
+		en = "Crucis Mk II Thunder Hammer",
+		["zh-tw"] = "II(二)型雷鎚(十字星)",
+	},
+	thunderhammer_2h_p1_m2 = {
+		en = "Ironhelm Mk IV Thunder Hammer",
+		["zh-tw"] = "IV(四)型雷鎚(鐵盔)",
+	},
+	chainaxe_p1_m1 = {
+		en = "Orestes Mk IV Assault Chainaxe",
+		["zh-tw"] = "IV(四)型突擊鏈斧(奧瑞斯特斯)",
+	},
+	chainaxe_p1_m2 = {
+		en = "Orestes Mk XII Assault Chainaxe",
+		["zh-tw"] = "XII(十二)型突擊鏈斧(奧瑞斯特斯)",
+	},
+	chainsword_p1_m1 = {
+		en = "Cadia Mk IV Assault Chainsword",
+		["zh-tw"] = "IV(四)型突擊鏈鋸劍(卡迪亞)",
+	},
+	chainsword_p1_m2 = {
+		en = "Cadia Mk XIIIg Assault Chainsword",
+		["zh-tw"] = "XIIIg(13G)型突擊鏈鋸劍(卡迪亞)",
+	},
+	combataxe_p1_m1 = {
+		en = "Rashad Mk III Combat Axe",
+		["zh-tw"] = "III(三)型戰鬥斧(拉沙德)",
+	},
+	combataxe_p1_m2 = {
+		en = "Antax Mk V Combat Axe",
+		["zh-tw"] = "V(五)型戰鬥斧(安塔克斯)",
+	},
+	combataxe_p1_m3 = {
+		en = "Achlys Mk VIII Combat Axe",
+		["zh-tw"] = "VIII(八)型戰鬥斧(阿克利斯)",
+	},
+	combataxe_p2_m1 = {
+		en = "Atrox Mk II Tactical Axe",
+		["zh-tw"] = "II(二)型戰術斧(埃托克斯)",
+	},
+	combataxe_p2_m2 = {
+		en = "Atrox Mk IV Tactical Axe",
+		["zh-tw"] = "IV(四)型戰術斧(埃托克斯)",
+	},
+	combataxe_p2_m3 = {
+		en = "Atrox Mk VII Tactical Axe",
+		["zh-tw"] = "VII(七)型戰術斧(埃托克斯)",
+	},
+	combatknife_p1_m1 = {
+		en = "Catachan Mk III Combat Knife",
+		["zh-tw"] = "III(三)型戰刃(卡塔昌)",
+	},
+	combatknife_p1_m2 = {
+		en = "Catachan Mk VI Combat Knife",
+		["zh-tw"] = "VI(六)型戰刃(卡塔昌)",
+	},
+	combatsword_p1_m1 = {
+		en = "Catachan Mk I 'Devil's Claw' Sword",
+		["zh-tw"] = "I(一)型「惡魔之爪」劍(卡塔昌)",
+	},
+	combatsword_p1_m2 = {
+		en = "Catachan Mk IV 'Devil's Claw' Sword",
+		["zh-tw"] = "IV(四)型「惡魔之爪」劍(卡塔昌)",
+	},
+	combatsword_p1_m3 = {
+		en = "Catachan Mk VII 'Devil's Claw' Sword",
+		["zh-tw"] = "VII(七)型「惡魔之爪」劍(卡塔昌)",
+	},
+	combatsword_p2_m1 = {
+		en = "Turtolsky Mk VI Heavy Sword",
+		["zh-tw"] = "VI(六)型重劍(圖妥斯基)",
+	},
+	combatsword_p2_m2 = {
+		en = "Turtolsky Mk VII Heavy Sword",
+		["zh-tw"] = "VII(七)型重劍(圖妥斯基)",
+	},
+	combatsword_p2_m3 = {
+		en = "Turtolsky Mk IX Heavy Sword",
+		["zh-tw"] = "IX(九)型重劍(圖妥斯基)",
+	},
+	combatsword_p3_m1 = {
+		en = "Maccabian Mk II Duelling Sword",
+		["zh-tw"] = "II(二)型決鬥劍(馬卡比安)",
+	},
+	combatsword_p3_m2 = {
+		en = "Maccabian Mk IV Duelling Sword",
+		["zh-tw"] = "IV(四)型決鬥劍(馬卡比安)",
+	},
+	combatsword_p3_m3 = {
+		en = "Maccabian Mk V Duelling Sword",
+		["zh-tw"] = "V(五)型決鬥劍(馬卡比安)",
+	},
+	powermaul_p1_m1 = {
+		en = "Agni Mk Ia Shock Maul",
+		["zh-tw"] = "Ia(1A)型電擊錘(阿格尼)",
+	},
+	powermaul_p1_m2 = {
+		en = "Munitorum Mk III Shock Maul",
+		["zh-tw"] = "III(三)型電擊錘(軍務部)",
+	},
+	-- RANGED
+	stubrevolver_p1_m1 = {
+		en = "Zaron Mk IIa Quickdraw Stub Revolver"
+	},
+	stubrevolver_p1_m2 = {
+		en = "Agripinaa Mk XIV Quickdraw Stub Revolver"
+	},
+	shotgun_p1_m1 = {
+		en = "Zarona Mk VI Combat Shotgun"
+	},
+	shotgun_p1_m2 = {
+		en = "Agripinaa Mk VII Combat Shotgun"
+	},
+	shotgun_p1_m3 = {
+		en = "Accatran Mk IX Combat Shotgun"
+	},
+	shotgun_p2_m1 = {
+		en = "Crucis Mk XI Double-Barrelled Shotgun"
+	},
+	plasmagun_p1_m1 = {
+		en = "M35 Magnacore Mk II Plasma Gun"
+	},
+	ogryn_thumper_p1_m1 = {
+		en = "Lorenz Mk V Kickback"
+	},
+	ogryn_thumper_p1_m2 = {
+		en = "Lorenz Mk VI Rumbler"
+	},
+	ogryn_rippergun_p1_m1 = {
+		en = "Foe-Rend Mk II Ripper Gun"
+	},
+	ogryn_rippergun_p1_m2 = {
+		en = "Foe-Rend Mk V Ripper Gun"
+	},
+	ogryn_rippergun_p1_m3 = {
+		en = "Foe-Rend Mk VI Ripper Gun"
+	},
+	ogryn_heavystubber_p1_m1 = {
+		en = "Krourk Mk V Twin-Linked Heavy Stubber"
+	},
+	ogryn_heavystubber_p1_m2 = {
+		en = "Gorgonum Mk IV Twin-Linked Heavy Stubber"
+	},
+	ogryn_heavystubber_p1_m3 = {
+		en = "Achlys Mk VII Twin-Linked Heavy Stubber"
+	},
+	ogryn_heavystubber_p2_m1 = {
+		en = "Krourk Mk IIa Heavy Stubber"
+	},
+	ogryn_heavystubber_p2_m2 = {
+		en = "Gorgonum Mk IIIa Heavy Stubber"
+	},
+	ogryn_heavystubber_p2_m3 = {
+		en = "Achlys Mk II Heavy Stubber"
+	},
+	laspistol_p1_m1 = {
+		en = "Accatran MG Mk II Heavy Laspistol"
+	},
+	laspistol_p1_m3 = {
+		en = "Kantrael Mk X Heavy Laspistol"
+	},
+	lasgun_p1_m1 = {
+		en = "Kantrael Mk VII Infantry Lasgun"
+	},
+	lasgun_p1_m2 = {
+		en = "Kantrael Mk IIb Infantry Lasgun"
+	},
+	lasgun_p1_m3 = {
+		en = "Kantrael Mk IX Infantry Lasgun"
+	},
+	lasgun_p2_m1 = {
+		en = "Lucius Mk IIIa Helbore Lasgun"
+	},
+	lasgun_p2_m2 = {
+		en = "Lucius Mk V Helbore Lasgun"
+	},
+	lasgun_p2_m3 = {
+		en = "Lucius Mk IV Helbore Lasgun"
+	},
+	lasgun_p3_m1 = {
+		en = "Accatran Mk VIc Recon Lasgun"
+	},
+	lasgun_p3_m2 = {
+		en = "Accatran Mk XII Recon Lasgun"
+	},
+	lasgun_p3_m3 = {
+		en = "Accatran Mk XIV Recon Lasgun"
+	},
+	forcestaff_p1_m1 = {
+		en = "Equinox Mk III Voidblast Force Staff"
+	},
+	forcestaff_p2_m1 = {
+		en = "Rifthaven Mk II Inferno Force Staff"
+	},
+	forcestaff_p3_m1 = {
+		en = "Nomanus Mk VI Electrokinetic Force Staff"
+	},
+	forcestaff_p4_m1 = {
+		en = "Equinox Mk IV Voidstrike Force Staff"
+	},
+	flamer_p1_m1 = {
+		en = "Artemia Mk III Purgation Flamer"
+	},
+	bolter_p1_m1 = {
+		en = "Locke Mk IIb Spearhead Boltgun"
+	},
+	boltpistol_p1_m1 = {
+		en = "Godwyn-Branx Mk IV Bolt Pistol"
+	},
+	autopistol_p1_m1 = {
+		en = "Ius Mk IV Shredder Autopistol"
+	},
+	autogun_p1_m1 = {
+		en = "Agripinaa Mk I Infantry Autogun"
+	},
+	autogun_p1_m2 = {
+		en = "Vraks Mk V Infantry Autogun"
+	},
+	autogun_p1_m3 = {
+		en = "Columnus Mk VIII Infantry Autogun"
+	},
+	autogun_p2_m1 = {
+		en = "Vraks Mk II Braced Autogun"
+	},
+	autogun_p2_m2 = {
+		en = "Graia Mk IV Braced Autogun"
+	},
+	autogun_p2_m3 = {
+		en = "Agripinaa Mk VIII Braced Autogun"
+	},
+	autogun_p3_m1 = {
+		en = "Columnus Mk III Heavy Autogun"
+	},
+	autogun_p3_m2 = {
+		en = "Graia Mk VII Heavy Autogun"
+	},
+	autogun_p3_m3 = {
+		en = "Agripinaa Mk IX Heavy Autogun"
+	},
+	psyker_throwing_knives = {
+		en = "Assail"
+	},
+}
+
+
