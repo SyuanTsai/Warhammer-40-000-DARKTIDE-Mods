b1a5ef449d498588a41d8b68ed1931710e1627f3
SyuanTsai <carsun00@Gmail.com>
2025-04-28 18:19:50 +0800

    Update

--- Warhammer 40,000 DARKTIDE/mods/clear_smoke/scripts/mods/clear_smoke/clear_smoke_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/clear_smoke/scripts/mods/clear_smoke/clear_smoke_localization.lua
@@ -1,26 +1,34 @@
 return {
 	mod_name = {
 		en = "Clear Smoke",
+		["zh-tw"] = "清除煙霧",
 	},
 	mod_description = {
 		en = "Disables smoke VFX and adds a radius indicator for smoke grenades.",
+		["zh-tw"] = "禁用煙霧視覺效果並為煙霧手榴彈添加半徑指示器。",
 	},
 	remove_smoke = {
 		en = "Remove Smoke Visual Effect",
+		["zh-tw"] = "移除煙霧視覺效果",
 	},
 	show_radius = {
 		en = "Show Radius Indicator",
+		["zh-tw"] = "顯示半徑指示器",
 	},
 	pulse_effect = {
 		en = "Pulse Effect in Last 5 Seconds",
+		["zh-tw"] = "最後5秒的脈衝效果",
 	},
 	color_r = {
 		en = "Radius Indicator Red",
+		["zh-tw"] = "半徑指示器紅色",
 	},
 	color_g = {
 		en = "Radius Indicator Green",
+		["zh-tw"] = "半徑指示器綠色",
 	},
 	color_b = {
 		en = "Radius Indicator Blue",
+		["zh-tw"] = "半徑指示器藍色",
 	},
 }
