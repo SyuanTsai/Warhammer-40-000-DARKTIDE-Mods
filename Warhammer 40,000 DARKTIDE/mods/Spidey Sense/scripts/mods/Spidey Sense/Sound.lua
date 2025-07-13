b69c77400114691c36b38683129e2482ba38354c
SyuanTsai <carsun00@Gmail.com>
2025-07-13 13:33:09 +0800

    Update.

--- Warhammer 40,000 DARKTIDE/mods/Spidey Sense/scripts/mods/Spidey Sense/Sound.lua
+++ Warhammer 40,000 DARKTIDE/mods/Spidey Sense/scripts/mods/Spidey Sense/Sound.lua
@@ -11,8 +11,7 @@ mod.sound.hooked_sounds = {
 	"wwise/events/minions/play_enemy_combat_poxwalker_bomber",	
   "wwise/events/player/play_backstab_indicator_melee",
 	"wwise/events/player/play_backstab_indicator_melee_elite",
-	"wwise/events/player/play_backstab_indicator_ranged",
-	"wwise/events/weapon/play_special_sniper_flash",
+	"wwise/events/player/play_backstab_indicator_ranged",	
 	"wwise/events/weapon/play_combat_weapon_las_sniper",
 	"wwise/events/minions/play_traitor_guard_grenadier",
 	"wwise/events/weapon/play_explosion_fuse",
@@ -43,7 +42,8 @@ mod.sound.hooked_sounds = {
   "wwise/events/weapon/play_minion_shotgun_pump",
   "wwise/events/minions/play_enemy_chaos_hound_vce_leap",
   "wwise/events/minions/play_chaos_hound_mutator_vce_leap",
-  "wwise/events/weapon/play_special_sniper_flash",
+  "wwise/events/weapon/play_special_sniper_flash",  
+  "wwise/events/weapon/play_weapon_longlas_minion",
   "spawn"
  }
 
