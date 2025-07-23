d420e3fd640550453e4984b4694b0848921fce87
SyuanTsai <carsun00@Gmail.com>
2025-07-23 17:58:05 +0800

    Add zh-tw

--- Warhammer 40,000 DARKTIDE/mods/valkyrie/scripts/mods/valkyrie/valkyrie_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/valkyrie/scripts/mods/valkyrie/valkyrie_localization.lua
@@ -2,17 +2,22 @@
 return {
   mod_name = {
     en = "Valkyrie Blitz Ingress",
+    ["zh-tw"] = "突擊連線",
   },
   mod_description = {
     en = "Load into in-progress missions faster by silencing briefings",
+    ["zh-tw"] = "透過靜音簡報來更快地進入進行中的任務",
   },
   MuteLobbyTitle = {
     en = "Also mute missions starting from lobby",
+    ["zh-tw"] = "也靜音從大廳開始的任務",
   },
   ReportOnOffTitle = {
     en = "Report map and difficulty level",
+    ["zh-tw"] = "報告地圖和難度級別",
   },
   MissionHologramTitle = {
     en = "Hide mission hologram",
+    ["zh-tw"] = "隱藏任務全息影像",
   },
 }
