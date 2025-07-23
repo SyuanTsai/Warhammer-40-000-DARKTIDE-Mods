3d95c44f7a8f8a02f1ba96f47f434f69795f9279
SyuanTsai <carsun00@Gmail.com>
2025-07-23 18:19:21 +0800

    Add Mods

---
+++ Warhammer 40,000 DARKTIDE/mods/BornReady/scripts/mods/BornReady/BornReady_localization.lua
@@ -0,0 +1,75 @@
+return {
+	mod_name = {
+		en = "BornReady",
+		ru = "Рождён готовым",
+		["zh-cn"] = "自动准备",
+	},
+	mod_description = {
+		en = "Options to auto-accept matchmaking, lobbies, and party invites.",
+		["zh-cn"] = "提供选项自动接受匹配、任务准备和小队邀请。",
+		ru = "Born Ready - Опции автовыбора в подборе, лобби и при приглашениях в группу.",
+	},
+	leave_party = {
+		en = Localize("loc_social_menu_leave_party"),
+	},
+	autoready = {
+		en = "Ready up in lobby",
+		["zh-cn"] = "在任务中准备",
+		ru = "Автоготовность в лобби",
+	},
+	autoskip = {
+		en = "Automatically skip",
+		["zh-cn"] = "跳过任务结束界面",
+		ru = "Пропустить экран окончания миссии",
+	},
+	end_skip_time = {
+		en = "Delay (seconds)",
+		["zh-cn"] = "跳过任务结束延迟（秒）",
+		ru = "Задержка перед пропуском экрана окончания миссии (сек)",
+	},
+	automatch = {
+		en = "Accept mission prompt",
+		["zh-cn"] = "接受任务请求",
+		ru = "Подтвердить приглашение на миссию",
+	},
+	autojoin = {
+		en = "Party invites",
+		["zh-cn"] = "小队邀请",
+		ru = "Приглашения в группу",
+	},
+	autowelcome = {
+		en = "Requests to join your party",
+		["zh-cn"] = "请求加入你的小队",
+		ru = "Просьбы присоединиться к вашей группе",
+	},
+	none = {
+		en = "Prompt",
+		["zh-cn"] = "提示",
+		ru = "Спрашивать",
+	},
+	auto_yes = {
+		en = "Automatically Accept",
+		["zh-cn"] = "自动接受",
+		ru = "Автоматически принимать",
+	},
+	auto_no = {
+		en = "Automatically Decline",
+		["zh-cn"] = "自动拒绝",
+		ru = "Автоматически отклонять",
+	},
+	eom_skip = {
+		en = Localize("loc_item_type_end_of_round"),
+	},
+	eom_skip_now_kb = {
+		en = "Skip Now",
+	},
+	eom_cancel_chat = {
+		en = "Chat cancels skip",
+	},
+	eom_cancel_kb = {
+		en = "Cancel Current Skip",
+	},
+	msg_eom_cancel = {
+		en = "End-of-mission autoskip cancelled",
+	},
+}
