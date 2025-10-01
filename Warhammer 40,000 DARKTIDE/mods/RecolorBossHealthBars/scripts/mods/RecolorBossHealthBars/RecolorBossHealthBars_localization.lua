dc70461a359053b0587532741a0914325041a466
SyuanTsai <carsun00@Gmail.com>
2025-10-01 18:35:03 +0800

    Update to 2025-09

--- Warhammer 40,000 DARKTIDE/mods/RecolorBossHealthBars/scripts/mods/RecolorBossHealthBars/RecolorBossHealthBars_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/RecolorBossHealthBars/scripts/mods/RecolorBossHealthBars/RecolorBossHealthBars_localization.lua
@@ -1,10 +1,12 @@
 local loc = {
 	mod_name = {
-        en = "Recolor Boss Health Bars",
+		en = "Recolor Boss Health Bars",
+		["zh-cn"] = "Boss血条上色",
 		["zh-tw"] = "重繪Boss血條",
-    },
+	},
 	mod_description = {
 		en = "Recolors the health bars of certain enemies, and allows more boss health bars to be shown on the screen at once",
+		["zh-cn"] = "重新给特定敌人的血条上色，并允许在屏幕上同时显示更多Boss血条",
 		["zh-tw"] = "重新染色敵人的血條，並允許螢幕上顯示更多的Boss血條",
 	},
 	color_daemonhost = {
@@ -29,28 +31,33 @@ local loc = {
 	},
 	color_weakened = {
 		en = "Weakened monsters",
+		["zh-cn"] = "虚弱怪物",
 		["zh-tw"] = "虛弱的怪物",
 	},
 	color_others = {
 		en = "Other monsters",
+		["zh-cn"] = "其他怪物",
 		["zh-tw"] = "其他怪物",
 	},
 	tooltip_color_toggle = {
 		en = "\nUse the specified color instead of the color defined in \"Other monsters\"",
+		["zh-cn"] = "\n使用指定颜色，而不是“其他怪物”中定义的颜色",
 		["zh-tw"] = "\n使用指定顏色，而不是“其他怪物”中定義的顏色",
 	},
 	lines_amount = {
 		en = "Max. number of lines of boss health bars",
+		["zh-cn"] = "最多Boss血条行数",
 		["zh-tw"] = "最大Boss血條行數",
 	},
 	columns_amount = {
-        en = "Number of boss health bars per line",
+		en = "Number of boss health bars per line",
+		["zh-cn"] = "每行Boss血条数量",
 		["zh-tw"] = "每行Boss血條數量",
 	},
 	tooltip_lines_amount = {
 		en = "\nMaximum number of lines of boss health bars that can be shown on screen at once.\n\nEach line contains two boss health bars.",
+		["zh-cn"] = "\n最多可以同时在屏幕上显示的Boss血条行数。\n\n每行包含两个Boss血条。",
 		["zh-tw"] = "\n一次可以在螢幕上顯示的最大Boss血條行數。\n\n每行包含兩個Boss血條。\n\n行數的最大值受螢幕高度限制。",
-
 	},
 	two = {
 		en = "2",
@@ -71,25 +78,37 @@ local unit_type_array = {
 	"others"
 }
 
-local loc_col = function(col)
-	if col == "alpha" then
-		return "Alpha"
-	elseif col == "r" then
-		return "R"
-	elseif col == "g" then
-		return "G"
-	elseif col == "b" then
-		return "B"
-	elseif col == "toggle" then
-		return "Use special color"
-	end
-end
+local loc_col = {
+	alpha = {
+		en = "Alpha",
+		["zh-cn"] = "不透明度",
+		["zh-tw"] = "透明度",
+	},
+	r = {
+		en = "R",
+		["zh-cn"] = "红色",
+		["zh-tw"] = "紅",
+	},
+	g = {
+		en = "G",
+		["zh-cn"] = "绿色",
+		["zh-tw"] = "綠",
+	},
+	b = {
+		en = "B",
+		["zh-cn"] = "蓝色",
+		["zh-tw"] = "藍",
+	},
+	toggle = {
+		en = "Use special color",
+		["zh-cn"] = "使用指定颜色",
+		["zh-tw"] = "使用指定顏色",
+	},
+}
 
 for _, unit_type in pairs(unit_type_array) do
 	for _, col in pairs({"alpha","r","g","b", "toggle"}) do
-		loc["color_"..unit_type.."_"..col] = {
-			en = loc_col(col)
-		}
+		loc["color_"..unit_type.."_"..col] = loc_col[col]
 	end
 end
 
