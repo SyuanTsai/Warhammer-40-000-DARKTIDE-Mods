ecb7aad31de4de42512bd58d55bf6e5e2beb2ada
SyuanTsai <carsun00@gmail.com>
2025-12-19 18:51:58 +0800

    Update to 2025-12-19

--- Warhammer 40,000 DARKTIDE/mods/DecodeMinigameSolver/scripts/DecodeMinigameSolver_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/DecodeMinigameSolver/scripts/DecodeMinigameSolver_localization.lua
@@ -33,10 +33,10 @@ return {
     ru = "Ожидание взаимодействия",
   },
   [SettingNames.InteractCooldownTooltip] = {
-    en = "Wait time between interactions in milliseconds. Default: 100",
-    ["zh-tw"] = "互動冷卻時間為毫秒 預設值為100",
-    ["zh-cn"] = "两次交互之间的等待时间（毫秒）。默认值：100",
-    ru = "Время ожидания между взаимодействиями в миллисекундах. По умолчанию: 100",
+    en = "Wait time between interactions in milliseconds. Default: 150",
+    ["zh-tw"] = "互動冷卻時間為毫秒 預設值為150",
+    ["zh-cn"] = "两次交互之间的等待时间（毫秒）。默认值：150",
+    ru = "Время ожидания между взаимодействиями в миллисекундах. По умолчанию: 150",
   },
   [SettingNames.TargetPrecision] = {
     en = "Target Precision",
@@ -45,9 +45,9 @@ return {
     ru = "Точность цели",
   },
   [SettingNames.TargetPrecisionTooltip] = {
-    en = "The bigger the value, the smaller the detection window when the cursor is positioned on the symbol. Default: 3",
+    en = "The bigger the value, the smaller the detection window when the cursor is positioned on the symbol. Default: 4",
     ["zh-tw"] = "當指標定位在符號上時 數值越大 偵測框越小 預設值為4",
-    ["zh-cn"] = "数值越大，当光标定位在符号上时，检测窗口越小。默认值：3",
-    ru = "Чем больше значение, тем меньше окно обнаружения, когда курсор находится на символе. По умолчанию: 3",
+    ["zh-cn"] = "数值越大，当光标定位在符号上时，检测窗口越小。默认值：4",
+    ru = "Чем больше значение, тем меньше окно обнаружения, когда курсор находится на символе. По умолчанию: 4",
   },
 }
