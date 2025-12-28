aa1a159e1d2b001113a4ddcc2e9ed0997c28c3a3
SyuanTsai <carsun00@gmail.com>
2025-12-28 13:01:03 +0800

    Add zh-tw

--- Warhammer 40,000 DARKTIDE/mods/vfx_swapper/scripts/mods/vfx_swapper/vfx_swapper_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/vfx_swapper/scripts/mods/vfx_swapper/vfx_swapper_localization.lua
@@ -2,146 +2,182 @@ return {
 	mod_name = {
 		en = "VFX Swapper",
 		["zh-cn"] = "爆炸视觉特效限制器",
+		["zh-tw"] = "爆炸視覺特效改善",
 	},
 	mod_description = {
 		en = "Replaces specific visual effects to improve visibility and performance.",
 		["zh-cn"] = "替换特定爆炸视觉特效以提升画面清晰度和游戏性能。",
+		["zh-tw"] = "替換特定爆炸特效以提升畫面清晰度和遊戲性能。",
 	},
 	vfx_replacement_group = {
 		en = "VFX Replacements",
 		["zh-cn"] = "特效替换",
+		["zh-tw"] = "特效替換",
 	},
 	replace_gas_vfx = {
 		en = "Replace Grenadier's Gas Bombs VFX",
 		["zh-cn"] = "替换毒气炸弹特效",
+		["zh-tw"] = "替換爆破手毒氣彈特效",
 	},
 	replace_renegade_grenade_vfx = {
 		en = "Replace Renegade Grenadier's Fire Grenade VFX",
 		["zh-cn"] = "替换叛军火焰手雷特效",
+		["zh-tw"] = "替換轟炸者火焰手雷特效",
 	},
 	replace_renegade_flamer_vfx = {
 		en = "Replace Renegade Flamer VFX",
 		["zh-cn"] = "替换叛军火焰喷射器特效",
+		["zh-tw"] = "替換血痂火焰噴射器特效",
 	},
 	replace_cultist_flamer_vfx = {
 		en = "Replace Cultist Flamer VFX",
 		["zh-cn"] = "替换狂信火焰喷射器特效",
+		["zh-tw"] = "替換渣梓火焰噴射器特效",
 	},
 	replace_blight_vfx = {
 		en = "Replace Havoc's Blight Spreads (ground) VFX",
 		["zh-cn"] = "替换腐化特效",
+		["zh-tw"] = "替換浩劫瘟疫擴散特效",
 	},
 	replace_immolation_vfx = {
 		en = "Replace Immolation Grenade VFX (Lingering Fire)",
 		["zh-cn"] = "替换狂信火雷特效（火焰）",
+		["zh-tw"] = "替換狂信火雷特效（火焰）",
 	},
 	replace_rotten_vfx = {
 		en = "Replace Rotten Armor's Lingering Gas (after kill) VFX",
 		["zh-cn"] = "替换腐化装甲特效",
+		["zh-tw"] = "替換腐化裝甲特效",
 	},	
 	gas_vfx_default = {
 		en = "Default",
 		["zh-cn"] = "預設值",
+		["zh-tw"] = "預設值",
 	},
 	gas_vfx_ground_cloud = {
 		en = "Ground Gas Cloud",
 		["zh-cn"] = "地面毒气云",
+		["zh-tw"] = "地面毒氣雲",
 	},
 	gas_vfx_green_flames = {
 		en = "Green Flames",
 		["zh-cn"] = "绿色火焰",
+		["zh-tw"] = "綠色火焰",
 	},
 	gas_vfx_beast_goo = {
 		en = "Beast Goo",
 		["zh-cn"] = "瘟疫兽粘液",
+		["zh-tw"] = "納垢獸黏液",
 	},
 	fire_vfx_default = {
 		en = "Default",
 		["zh-cn"] = "預設值",
+		["zh-tw"] = "預設值",
 	},
 	fire_vfx_beast_slime = {
 		en = "Beast Slime",
 		["zh-cn"] = "瘟疫兽黏液",
+		["zh-tw"] = "納垢獸黏液",
 	},
 	fire_vfx_beast_goo = {
 		en = "Beast Goo",
 		["zh-cn"] = "瘟疫兽腐液",
+		["zh-tw"] = "納垢獸腐液",
 	},
 	fire_short = {
 		en = "Short Red Fire",
 		["zh-cn"] = "短效红色火焰",
+		["zh-tw"] = "短效紅色火焰",
 	},
 	green_fire_short = {
 		en = "Short Green Fire",
 		["zh-cn"] = "短效绿色火焰",
+		["zh-tw"] = "短效綠色火焰",
 	},
 	druglab_tank_goo = {
 		en = "Druglab Tank Goo",
 		["zh-cn"] = "毒品实验室罐子腐液",
+		["zh-tw"] = "毒品實驗室罐子腐液",
 	},
 	summoning_circle = {
 		en = "Flat Black Cicles",
 		["zh-cn"] = "黑色召唤阵",
+		["zh-tw"] = "黑色召喚陣",
 	},
 	rotten_vfx_default = {
 		en = "Default",
 		["zh-cn"] = "預設值",
+		["zh-tw"] = "預設值",
 	},
 	flamer_vfx_default = {
 		en = "Default",
 		["zh-cn"] = "預設值",
+		["zh-tw"] = "預設值",
 	},
 	grenade_vfx_default = {
 		en = "Default",
 		["zh-cn"] = "預設值",
+		["zh-tw"] = "預設值",
 	},
 	blight_vfx_default = {
 		en = "Default",
 		["zh-cn"] = "預設值",
+		["zh-tw"] = "預設值",
 	},
 	circle_indicators_group = {
 		en = "Circle Indicators",
 		["zh-cn"] = "圆圈指示器",
+		["zh-tw"] = "圓圈指示器",
 	},
 	rotten_circle_enabled = {
 		en = "Rotten Armor Circle",
 		["zh-cn"] = "腐化装甲圆圈",
+		["zh-tw"] = "腐化裝甲圓圈",
 	},
 	rotten_circle_red = {
 		en = "Rotten - Red",
 		["zh-cn"] = "腐化 - 红",
+		["zh-tw"] = "腐化 - 紅",
 	},
 	rotten_circle_green = {
 		en = "Rotten - Green",
 		["zh-cn"] = "腐化 - 绿",
+		["zh-tw"] = "腐化 - 綠",
 	},
 	rotten_circle_blue = {
 		en = "Rotten - Blue",
 		["zh-cn"] = "腐化 - 蓝",
+		["zh-tw"] = "腐化 - 藍",
 	},
 	rotten_circle_alpha = {
 		en = "Rotten - Opacity",
 		["zh-cn"] = "腐化 - 透明度",
+		["zh-tw"] = "腐化 - 透明度",
 	},
 	blight_circle_enabled = {
 		en = "Blight Spreads Circle",
 		["zh-cn"] = "瘟疫扩散圆圈",
+		["zh-tw"] = "瘟疫擴散圓圈",
 	},
 	blight_circle_red = {
 		en = "Blight - Red",
 		["zh-cn"] = "瘟疫 - 红",
+		["zh-tw"] = "瘟疫 - 紅",
 	},
 	blight_circle_green = {
 		en = "Blight - Green",
 		["zh-cn"] = "瘟疫 - 绿",
+		["zh-tw"] = "瘟疫 - 綠",
 	},
 	blight_circle_blue = {
 		en = "Blight - Blue",
 		["zh-cn"] = "瘟疫 - 蓝",
+		["zh-tw"] = "瘟疫 - 藍",
 	},
 	blight_circle_alpha = {
 		en = "Blight - Opacity",
 		["zh-cn"] = "瘟疫 - 透明度",
+		["zh-tw"] = "瘟疫 - 透明度",
 	},
 }
 
