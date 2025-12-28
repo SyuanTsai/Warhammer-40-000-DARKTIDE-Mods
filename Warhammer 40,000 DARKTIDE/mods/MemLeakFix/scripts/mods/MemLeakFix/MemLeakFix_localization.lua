fa07c1bdd001d392b43be7fb8df55083cf0386c8
SyuanTsai <carsun00@gmail.com>
2025-12-28 13:06:32 +0800

    Add zh-tw

--- Warhammer 40,000 DARKTIDE/mods/MemLeakFix/scripts/mods/MemLeakFix/MemLeakFix_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/MemLeakFix/scripts/mods/MemLeakFix/MemLeakFix_localization.lua
@@ -6,22 +6,27 @@ return {
 	mod_name = {
 		en = "MemLeakFix",
 		["zh-cn"] = "内存泄漏修复",
+		["zh-tw"] = "記憶體使用改善",
 	},
 	mod_description = {
 		en = "Try to fix Lua VM out of memory error by using aggressive strategy of Lua GC",
 		["zh-cn"] = "尝试通过更激进的GC行为修复内存溢出",
+		["zh-tw"] = "嘗試通過更積極的GC行為修復記憶體溢出",
 	},
 	pause_time = {
 		en = "GC pause time",
 		["zh-cn"] = "GC 间歇率",
+		["zh-tw"] = "GC 回收頻率",
 	},
 	step_mul = {
 		en = "GC step multiple",
 		["zh-cn"] = "GC 步进率",
+		["zh-tw"] = "GC 執行強度(會增加效能消耗)",
 	},
 	memory = {
 		en = "LuaMem",
 		["zh-cn"] = "Lua内存",
+		["zh-tw"] = "Ram",
 	},
 	-- gc_monitor = {
 	-- 	en = "GC Monitor switch",
