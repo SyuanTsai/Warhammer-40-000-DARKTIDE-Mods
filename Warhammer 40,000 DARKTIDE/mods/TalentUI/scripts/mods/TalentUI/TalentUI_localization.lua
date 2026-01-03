34b269f9cdd74b982aa9eca6032c7381919fb40c
SyuanTsai <carsun00@gmail.com>
2026-01-03 13:16:06 +0800

    Add zh-tw

--- Warhammer 40,000 DARKTIDE/mods/TalentUI/scripts/mods/TalentUI/TalentUI_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/TalentUI/scripts/mods/TalentUI/TalentUI_localization.lua
@@ -2,66 +2,82 @@ return {
 	mod_name = {
 		en = "TalentUI",
 		ru = "TalentUI",
+		["zh-tw"] = "天賦UI",
 	},
 	mod_description = {
 		en = "Shows ability icons with cooldown timers for teammates and local player in the team HUD",
 		ru = "Показывает иконки способностей с таймерами кулдауна для тимейтов и локального игрока в team HUD",
+		["zh-tw"] = "在隊伍HUD中顯示所有玩家的技能圖示和冷卻計時器",
 	},
 	group_teammate_ability = {
 		en = "Teammate Ability",
 		ru = "Способность тимейтов",
+		["zh-tw"] = "隊友技能",
 	},
 	group_teammate_blitz = {
 		en = "Teammate Blitz",
 		ru = "Blitz тимейтов",
+		["zh-tw"] = "隊友閃擊",
 	},
 	group_teammate_aura = {
 		en = "Teammate Aura",
 		ru = "Аура тимейтов",
+		["zh-tw"] = "隊友光環",
 	},
 	group_local_player = {
 		en = "Local Player",
 		ru = "Локальный игрок",
+		["zh-tw"] = "自身",
 	},
 	show_teammate_ability_icon = {
 		en = "Show Ability Icons",
 		ru = "Показывать иконки способностей",
+		["zh-tw"] = "顯示技能圖示",
 	},
 	show_teammate_ability_cooldown = {
 		en = "Show Cooldown",
 		ru = "Показывать кулдаун",
+		["zh-tw"] = "顯示冷卻時間",
 	},
 	show_teammate_blitz_icon = {
 		en = "Show Blitz Icons",
 		ru = "Показывать иконки blitz",
+		["zh-tw"] = "顯示閃擊圖示",
 	},
 	show_teammate_blitz_charges = {
 		en = "Show Charges",
 		ru = "Показывать заряды",
+		["zh-tw"] = "顯示數量",
 	},
 	show_teammate_aura_icon = {
 		en = "Show Aura Icons",
 		ru = "Показывать иконки ауры",
+		["zh-tw"] = "顯示光環圖示",
 	},
 	show_local_ability_cooldown = {
 		en = "Show Cooldown",
 		ru = "Показывать кулдаун",
+		["zh-tw"] = "顯示冷卻時間",
 	},
 	cooldown_format = {
 		en = "Display Format",
 		ru = "Формат отображения",
+		["zh-tw"] = "顯示格式",
 	},
 	cooldown_format_time = {
 		en = "Time (seconds)",
 		ru = "Время (секунды)",
+		["zh-tw"] = "時間（秒）",
 	},
 	cooldown_format_percent = {
 		en = "Percent",
 		ru = "Процент",
+		["zh-tw"] = "百分比",
 	},
 	local_cooldown_font_size = {
 		en = "Font Size",
 		ru = "Размер шрифта",
+		["zh-tw"] = "字體大小",
 	},
 }
 
