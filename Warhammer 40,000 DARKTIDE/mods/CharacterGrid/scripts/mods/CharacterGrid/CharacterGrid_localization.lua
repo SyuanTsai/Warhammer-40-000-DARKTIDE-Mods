37f5b7f7a0833dd9ff4b96cb08b845e3078b111a
SyuanTsai <carsun00@gmail.com>
2026-07-04 23:01:44 +0800

    Translate zh-tw localization for CharacterGrid

--- Warhammer 40,000 DARKTIDE/mods/CharacterGrid/scripts/mods/CharacterGrid/CharacterGrid_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/CharacterGrid/scripts/mods/CharacterGrid/CharacterGrid_localization.lua
@@ -3,13 +3,13 @@ return {
 		en = "CharacterGrid",
 		ru = "Сетка персонажей",
 		["zh-cn"] = "角色选择重新排序",
-		["zh-tw"] = "角色選擇重新排序",
+		["zh-tw"] = "角色網格",
 	},
 	mod_description = {
 		en = "Make the character selection screen better",
 		ru = "CharacterGrid - Улучшите экран выбора персонажей.",
 		["zh-cn"] = "优化角色选择界面",
-		["zh-tw"] = "優化角色選擇介面",
+		["zh-tw"] = "改善角色選擇畫面",
 	},
 	character_count = {
 		en = "Number of characters to display in grid",
