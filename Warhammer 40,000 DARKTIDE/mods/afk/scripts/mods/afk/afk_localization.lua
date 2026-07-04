0d6fd497d75e28e517911249a66fb5965dfb373b
SyuanTsai <carsun00@gmail.com>
2026-07-04 12:37:48 +0800

    Translate zh-tw localization for afk

--- Warhammer 40,000 DARKTIDE/mods/afk/scripts/mods/afk/afk_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/afk/scripts/mods/afk/afk_localization.lua
@@ -8,19 +8,19 @@ return {
   mod_description = {
     en = "Delays the server AFK timer for a set number of checks.",
     ["zh-cn"] = "延后一定次数的服务器挂机检查。",
-    ["zh-tw"] = "延遲伺服器AFK計時器的檢查次數",
+    ["zh-tw"] = "延後伺服器 AFK 計時器，持續指定檢查次數。",
     ru = "Откладывает AFK-таймер сервера на заданное количество проверок.",
   },
   block_check_limit = {
     en = "Block Check Limit",
     ["zh-cn"] = "阻止检查上限",
-    ["zh-tw"] = "阻止檢查上限",
+    ["zh-tw"] = "阻擋檢查上限",
     ru = "Количество проверок",
   },
   block_check_limit_description = {
     en = "Maximum number of prevented checks.",
     ["zh-cn"] = "阻止挂机检查的最大次数。",
-    ["zh-tw"] = "阻止檢查的最大次數",
+    ["zh-tw"] = "可阻擋檢查的最大次數。",
     ru = "Максимальное количество пропущенных проверок.",
   },
 }
