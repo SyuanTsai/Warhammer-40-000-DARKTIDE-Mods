ab8e16e5fd7a24981d9e1b88bc7ace4414220e83
SyuanTsai <carsun00@gmail.com>
2026-07-05 20:00:01 +0800

    Update to 20260705

--- Warhammer 40,000 DARKTIDE/mods/TagKeys/scripts/mods/TagKeys/TagKeys_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/TagKeys/scripts/mods/TagKeys/TagKeys_localization.lua
@@ -1,25 +1,25 @@
-return {
+local mod = get_mod("TagKeys")
+
+mod.tags = {
+	cheer = "loc_communication_wheel_display_name_cheer",
+	health = "loc_communication_wheel_display_name_need_health",
+	thanks = "loc_communication_wheel_display_name_thanks",
+	ammo = "loc_communication_wheel_display_name_need_ammo",
+	enemy = "loc_communication_wheel_display_name_enemy",
+	location = "loc_communication_wheel_display_name_location",
+	attention = "loc_communication_wheel_display_name_attention",
+}
+
+local localization = {
 	mod_description = {
 		en = "Bind individual tagging wheel options to keys.",
 		["zh-cn"] = "将单个标签轮盘操作绑定到独立按键上。",
-		["zh-tw"] = "將單個標籤輪盤操作綁定到快捷按鍵上。",
-	},
-	key_thanks = {
-		en = Localize("loc_communication_wheel_display_name_thanks"),
-	},
-	key_need_health = {
-		en = Localize("loc_communication_wheel_display_name_need_health"),
-	},
-	key_enemy = {
-		en = Localize("loc_communication_wheel_display_name_enemy"),
-	},
-	key_location = {
-		en = Localize("loc_communication_wheel_display_name_location"),
-	},
-	key_attention = {
-		en = Localize("loc_communication_wheel_display_name_attention"),
-	},
-	key_need_ammo = {
-		en = Localize("loc_communication_wheel_display_name_need_ammo"),
+		["zh-tw"] = "將輪盤標記操作綁定到獨立按鍵上。",
 	},
 }
+
+for id, loc in pairs(mod.tags) do
+	localization["key_" .. id] = { en = Localize(loc) }
+end
+
+return localization
