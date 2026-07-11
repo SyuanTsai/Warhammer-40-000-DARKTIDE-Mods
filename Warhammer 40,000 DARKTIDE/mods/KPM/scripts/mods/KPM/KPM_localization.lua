9a92cf3a2940f8ff05686f2eaf155a4ec860214b
SyuanTsai <carsun00@gmail.com>
2026-07-12 02:55:22 +0800

    Translate zh-tw localization for KPM

--- Warhammer 40,000 DARKTIDE/mods/KPM/scripts/mods/KPM/KPM_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/KPM/scripts/mods/KPM/KPM_localization.lua
@@ -5,18 +5,18 @@ return {
 	},
 	mod_description = {
 		en = "[Warhammer 40K Darktide Competition Mod] Displays team KPM data - Custom development: deluxghost",
-		["zh-tw"] = "[戰鎚40K暗潮吧競賽模組] 顯示全隊 KPM 數據 - 客製化開發：deluxghost",
+		["zh-tw"] = "[戰錘40K黑潮競賽模組] 顯示團隊 KPM 資料 - 客製化開發：deluxghost",
 	},
 	show_mkpm = {
 		en = "Show Melee Elite KPM",
-		["zh-tw"] = "顯示近戰精英 KPM",
+		["zh-tw"] = "顯示近戰類精英 KPM",
 	},
 	show_rkpm = {
 		en = "Show Ranged Elite KPM",
-		["zh-tw"] = "顯示遠程精英 KPM",
+		["zh-tw"] = "顯示遠程類精英 KPM",
 	},
 	show_skpm = {
 		en = "Show Specialist KPM",
-		["zh-tw"] = "顯示專家 KPM",
+		["zh-tw"] = "顯示專家敵人 KPM",
 	},
 }
