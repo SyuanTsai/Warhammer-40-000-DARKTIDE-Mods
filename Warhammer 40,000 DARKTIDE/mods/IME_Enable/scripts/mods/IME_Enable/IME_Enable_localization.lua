4beb2fc3d75840bc4303239948bceb9d16e637ab
SyuanTsai <carsun00@gmail.com>
2026-07-13 19:11:01 +0800

    Translate zh-tw localization for IME_Enable

--- Warhammer 40,000 DARKTIDE/mods/IME_Enable/scripts/mods/IME_Enable/IME_Enable_localization.lua
+++ Warhammer 40,000 DARKTIDE/mods/IME_Enable/scripts/mods/IME_Enable/IME_Enable_localization.lua
@@ -9,7 +9,7 @@ return {
 			"Some input methods themselves may not compatible with Darktide, which is not fixable by this mod.",
 		["zh-cn"] = "为游戏内文本框启用 Windows 输入法支持。适用于中日韩等语言的玩家。\n\n"..
 			"一些输入法自身不兼容暗潮，此模组无法解决此类问题。",
-		["zh-tw"] = "為遊戲內文字框啟用 Windows 輸入法支援。適用於中日韓等語言的玩家。\n\n" ..
-			"某些輸入法本身可能與《Darktide》不相容，此模組無法修正此類問題。",
+		["zh-tw"] = "為遊戲內文字輸入框啟用 Windows 輸入法支援。適合使用中日韓等語言的玩家。\n\n" ..
+			"某些輸入法本身可能與《黑潮》不相容，此模組無法修正此類問題。",
 	},
 }
